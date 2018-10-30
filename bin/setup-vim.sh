905255b0e6553a81aea0a483842f601d2c240133
Wander Lairson Costa <wcosta@mozilla.com>
2018-10-30 16:42:43 -0300

    Better golang support in vim

--- bin/setup-vim.sh
+++ bin/setup-vim.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -ve
 
+go get -u github.com/mdempsky/gocode
+go get -u github.com/alecthomas/gometalinter
+
 vim -e +BundleInstall +qall || :
+
 $HOME/work/vimfiles/bundle/YouCompleteMe/install.py \
     --clang-completer \
-    --gocode-completer \
     --tern-completer \
     --system-libclang
+
 vim -e +GoInstallBinaries +qall
-go get -u github.com/alecthomas/gometalinter
 gometalinter --install
 
 cd $HOME/work/vimfiles/bundle/tern_for_vim/
