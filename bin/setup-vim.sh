2b67335f4510f369a9cd306049c8c3b6154f94f5
Wander Lairson Costa <wander.lairson@gmail.com>
2020-06-11 15:49:03 -0300

    Add coc.nvim
    Use coc.nvim for all languages but golang, which I couldn't make work,
    so let's keep YCM for Go.

--- bin/setup-vim.sh
+++ bin/setup-vim.sh
@@ -3,14 +3,21 @@
 : WORK=${WORK=$HOME/work}
 
 vim -e +BundleInstall +qall || :
+vim -e +"CocInstall \
+    coc-marketplace \
+    coc-json \
+    coc-cmake \
+    coc-explorer \
+    coc-python \
+    coc-rls \
+    coc-yaml \
+    coc-clangd \
+    coc-tsserver \
+    "
 
-$WORK/vimfiles/bundle/YouCompleteMe/install.py \
-    --clang-completer \
-    --clangd-completer \
-    --tern-completer \
-    --ts-completer \
-    --go-completer \
-    --rust-completer
+$WORK/vimfiles/bundle/YouCompleteMe/install.py --go-completer
+
+vim -e +GoInstallBinaries +qall
 
 cd $WORK/vimfiles/bundle/tern_for_vim/
 npm install
