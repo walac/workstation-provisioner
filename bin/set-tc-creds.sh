6d2a4e5cc60b10e05c032a2320718690169c7ae1
Wander Lairson Costa <wander.lairson@gmail.com>
2020-07-03 11:15:54 -0300

    Remove unused scripts

--- bin/set-tc-creds.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export TASKCLUSTER_ACCESS_TOKEN=$(pass show community-tc/root | head -1)
-export TASKCLUSTER_CLIENT_ID=$(pass show community-tc/root | tail -1)
-export TASKCLUSTER_ROOT_URL=https://community-tc.services.mozilla.com
